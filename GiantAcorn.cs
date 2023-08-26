9859b76330351c433a460b9adc7aff431b52edf3
BrandonJack1 <Brandonstewartjack@gmail.com>
2023-08-26 14:46:16 -0300

    Refactored GiantAcorn.cs

--- GiantAcorn.cs
+++ GiantAcorn.cs
@@ -4,40 +4,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-
 public class GiantAcorn : MonoBehaviour
 {
-    public Animator animator;
-    public ParticleSystem leaves;
-    public AudioSource source;
-    public AudioClip thud;
-
-    public GameObject cam;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private Animator animator;
+    [SerializeField] private  ParticleSystem leaves;
+    [SerializeField] private  AudioSource source;
+    [SerializeField] private  AudioClip thud;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
+        //if the ground detects a giant acorn
         if (col.CompareTag("GiantAcorn"))
         {
-            
+            //play thud
             source.PlayOneShot(thud);
+            
+            //reset leaves particles and play it
             leaves.Clear();
             leaves.Play();
+            
+            //play screen shake animaiton
             animator.SetTrigger("Shake");
-            //Make leaves particle system active
-
         }
     }
-
-    
 }
